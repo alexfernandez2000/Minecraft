46495b53a9403b5c561fb3688928483aa11f5fd4
xalex <xalex@DESKTOP-HV0RP1K>
2024-07-10 18:52:15 +0200

    Agregar archivos de proyecto.

---
+++ Minecraft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Minecraft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
