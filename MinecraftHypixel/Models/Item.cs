80afe5df18b524439e8cd0cd2ba3b98b0ce63eb8
alexFernandez <70752172+alexfernandez2000@users.noreply.github.com>
2024-07-26 18:03:25 +0200

    FormGenerico funcional aunque no bonito

--- MinecraftHypixel/Models/Item.cs
+++ MinecraftHypixel/Models/Item.cs
@@ -2,8 +2,11 @@
 
 namespace MinecraftHypixel.Models
 {
-    public class Item : ItemView
+    public class Item
     {
+        [Required(ErrorMessage = "Id is required")]
+        public string? Id { get; set; }
+        public string? Name { get; set; }
         public string? Material {get;set;}
         public string? Color { get; set; }
         public string? Category { get; set; }
