ae4b4af2e15bf6f987850cfc96ef6fa1bb920283
alexFernandez <70752172+alexfernandez2000@users.noreply.github.com>
2024-07-26 14:19:05 +0200

    Partial generic form

---
+++ MinecraftHypixel/Components/Pages/Common/StringTools.cs
@@ -0,0 +1,12 @@
+﻿namespace MinecraftHypixel.Components.Pages.Common
+{
+    public static class StringTools
+    {
+        public static IDictionary<string, string> ValueList = new Dictionary<string, string>() 
+        { 
+            {"Int32","number"},
+            {"String","text" }
+        };
+        
+    }
+}
